ec24538c95047bc6aee4c2a0a454a7da27bba016
NaHCO314 <73940286+NaHCO314@users.noreply.github.com>
2021-01-11 01:57:55 +0900

    Update search.py

--- graph/search.py
+++ graph/search.py
@@ -8,21 +8,6 @@ def DFS(g, s, pos=None):
     return pos
 
 
-def DFS_one(g, s, pos=None):
-    if pos is None:
-        pos = set()
-    pos = copy(pos)
-    pos.add(s)
-    b = copy(pos)
-    m = copy(pos)
-    for i in g[s]:
-        if not (i in pos):
-            b = DFS(g, i, pos)
-            if len(m) < len(b):
-                m = b
-    return m
-
-
 def BFS(g, q, pos=None):
     if pos is None:
         pos = set()
