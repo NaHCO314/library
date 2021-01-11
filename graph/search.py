43e531a6a0a9ce81cb7bd7c72950a96b69fdcd63
NaHCO314 <73940286+NaHCO314@users.noreply.github.com>
2021-01-11 18:29:29 +0900

    Update search.py

--- graph/search.py
+++ graph/search.py
@@ -8,16 +8,17 @@ def DFS(g, s, pos=None):
     return pos
 
 
-def BFS(g, q, pos=None):
-    if pos is None:
-        pos = set()
-    if type(q) == deque:
-        pos.add(q)
-        q = deque([q])
-    pos.add(q[-1])
-    for i in g[q.pop()]:
-        if not i in pos:
-            q.append(i)
-    while q != deque():
-        pos, q = BFS(g, q, pos)
-    return pos, q
+def BFS(g, s):
+    q, pos = collections.deque([s]), set([s])
+    while q:
+        s = q.pop()
+        for i in g[s]:
+            if i not in pos:
+                pos.add(i)
+                q.append(i)
+    return pos
+
+# 隣接リスト
+# D/BFS(グラフ, スタート地点)
+# 戻り値はスタート地点と連結している要素のset
+
