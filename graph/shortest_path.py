4e09397980fe1be7bf5254982e7ed9ab714bbbeb
NaHCO314 <73940286+NaHCO314@users.noreply.github.com>
2021-01-12 18:15:59 +0900

    Update shortest_path.py

--- graph/shortest_path.py
+++ graph/shortest_path.py
@@ -2,18 +2,36 @@ def dijkstra(g, s):
     n = len(g)
     dist = [float("INF")] * n
     dist[s] = 0
-    hq = [(0, s)]
+    q = [(0, s)]
     seen = [False] * n
-    while hq:
-        v = heapq.heappop(hq)[1]
+    while q:
+        v = heapq.heappop(q)[1]
         seen[v] = True
-        for to, cost in g[v]:
-            if seen[to] == False and dist[v] + cost < dist[to]:
-                dist[to] = dist[v] + cost
-                heapq.heappush(hq, (dist[to], to))
+        for i, j in g[v]:
+            if seen[i] == False and dist[v] + j < dist[i]:
+                dist[i] = dist[v] + j
+                heapq.heappush(q, (dist[i], i))
     return dist
 
 # dijkstra(グラフ, 開始地点)
 # 戻り値はリストで、[i]にはiまでの最短コストがある
 # 隣接リスト [[[ノード, コスト]...]...]
 # O((E+V) log V)
+
+def BFS(g, s):
+    dist = [float("INF")] * len(g)
+    dist[s] = 0
+    q, pos = collections.deque([s]), [False] * len(g)
+    while q:
+        s = q.popleft()
+        for i in g[s]:
+            if not pos[i]:
+                dist[i] = dist[s] + 1
+                pos[i] = True
+                q.append(i)
+    return dist
+
+# BFS(グラフ、開始地点)
+# 戻り値はリストで、[i]にはiまでの最短距離がある
+# 隣接リスト [[ノード...]...]
+# O(E)
