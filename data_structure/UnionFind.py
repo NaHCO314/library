16c4caa5096f7395d73ab029b1f5a36c7745dbe9
NaHCO314 <73940286+NaHCO314@users.noreply.github.com>
2021-01-25 21:19:19 +0900

    Update UnionFind.py

--- data_structure/UnionFind.py
+++ data_structure/UnionFind.py
@@ -1,40 +1,40 @@
 class UnionFind:
     def __init__(self, n):
-        self.n = n
-        self.parents = [-1 for i in range(n)]
+        self.__n = n
+        self.__parents = [-1] * n
 
     def union(self, x, y):
         x, y = self.find(x), self.find(y)
         if x != y:
-            if self.parents[x] > self.parents[y]:
+            if self.__parents[x] > self.__parents[y]:
                 x, y = y, x
-            self.parents[x] += self.parents[y]
-            self.parents[y] = x
+            self.__parents[x] += self.__parents[y]
+            self.__parents[y] = x
 
     def find(self, x):
         y = x
-        while self.parents[x] >= 0:
-            x = self.parents[x]
+        while self.__parents[x] >= 0:
+            x = self.__parents[x]
         while y != x:
             t = y
-            y = self.parents[y]
-            self.parents[t] = x
+            y = self.__parents[y]
+            self.__parents[t] = x
         return x
 
     def size(self, x):
-        return -self.parents[self.find(x)]
+        return -self.__parents[self.find(x)]
 
     def same(self, x, y):
          return self.find(x) == self.find(y)
 
     def members(self, x):
         root = self.find(x)
-        return [i for i in range(len(self.parents)) if self.find(i) == root]
+        return [i for i in range(len(self.__parents)) if self.find(i) == root]
 
     def roots(self):
         ans = []
-        for i in range(self.n):
-            if self.parents[i] < -1:
+        for i in range(self.__n):
+            if self.__parents[i] < -1:
                 ans += [i]
         return ans
 
@@ -42,10 +42,40 @@ class UnionFind:
         return len(self.roots())
 
     def max_size(self):
-        return -min(self.parents)
+        return -min(self.__parents)
 
     def min_size(self):
         return -max(self.roots())
 
-    def append(self, n):
-        self.parents += [-1 for i in range(n)]
+
+class WeightedUnionFind(UnionFind):
+    def __init__(self, n):
+        self.__weight = [0] * n
+        self.__n = n
+        self.__parents = [-1 for i in range(n)]
+
+    def union(self, x, y, w):
+        w += self.weight(x) - self.weight(y)
+        x, y = self.find(x), self.find(y)
+        if x != y:
+            if self.__parents[x] > self.__parents[y]:
+                x, y, w = y, x, -w
+            self.__parents[x] += self.__parents[y]
+            self.__parents[y], self.__weight[y] = x, w
+
+    def find(self, x):
+        s = []
+        while self.__parents[x] >= 0:
+            s += [x]
+            x = self.__parents[x]
+        for i in s[::-1]:
+            self.__weight[i] += self.__weight[self.__parents[i]]
+            self.__parents[i] = x
+        return x
+
+    def weight(self, x):
+        self.find(x)
+        return self.__weight[x]
+
+    def diff(self, x, y):
+        return self.weight(y) - self.weight(x)
