a7fa6c7f358511a32eb41e3967c7b28099355417
NaHCO314 <73940286+NaHCO314@users.noreply.github.com>
2021-02-05 01:51:51 +0900

    Update DisjointSparseTable.py

--- data_structure/DisjointSparseTable.py
+++ data_structure/DisjointSparseTable.py
@@ -1,27 +1,26 @@
 class DisjointSparseTable:
     def __init__(self, vector, op):
-        self.vector = vector
-        self.op = op
+        self.__vector = vector
+        self.__op = op
         vs = len(vector)
-        n = vs.bit_length() - (vs.bit_length() != (vs-1).bit_length())
-        self.value = [vector] + [[0] * vs for i in range(n-1)]
+        n = vs.bit_length() - (vs.bit_length() != (vs - 1).bit_length())
+        self.__value = [vector] + [[0] * vs for i in range(n - 1)]
         for i in range(1, n):
             s = 1 << i
             j = 0
             while j < vs:
                 m = min(j + s, vs)
-                self.value[i][m - 1] = vector[m - 1]
-                for k in range(m-2, j-1, -1):
-                    self.value[i][k] = self.op(self.vector[k], self.value[i][k+1])
+                self.__value[i][m - 1] = vector[m - 1]
+                for k in range(m - 2, j - 1, -1):
+                    self.__value[i][k] = self.__op(self.__vector[k], self.__value[i][k + 1])
                 if vs > m:
-                    self.value[i][m] = self.vector[m]
-                    for k in range(m+1, min(m + s, vs)):
-                        self.value[i][k] = self.op(self.value[i][k-1], self.vector[k])
+                    self.__value[i][m] = self.__vector[m]
+                    for k in range(m + 1, min(m + s, vs)):
+                        self.__value[i][k] = self.__op(self.__value[i][k - 1], self.__vector[k])
                 j += s << 1
 
     def query(self, l, r):
         if l == r:
-            return self.vector[l]
-        p = -len(self.value) + (l ^ r).bit_length() - 1
-        return self.op(self.value[p][l], self.value[p][r])
-    
+            return self.__vector[l]
+        p = -len(self.__value) + (l ^ r).bit_length() - 1
+        return self.__op(self.__value[p][l], self.__value[p][r])
